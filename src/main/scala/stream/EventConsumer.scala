0970da1929ae1f589aee04c01571241a98053009
Dylan Andrade <andrademdylan@gmail.com>
2026-03-15 19:35:22 +0100

    Add error handling

--- src/main/scala/stream/EventConsumer.scala
+++ src/main/scala/stream/EventConsumer.scala
@@ -17,8 +17,22 @@ object EventConsumer:
       // Added a sleep to simulate processing time.
       // NOTE: I'm using IO.sleep here to avoid blocking the thread, which allows other events to be processed concurrently.
       _ <- IO.sleep(2.seconds)
+      _ <-
+        if event.payload.contains("fail") then
+          IO.raiseError(new RuntimeException(s"processing failed for event ${event.id.value}"))
+        else IO.unit
       _ <- IO.println(s"[consumer] finished processing event ${event.id.value}")
     yield ()
+
+  private def processSafely(event: Event): IO[Unit] = {
+    // NOTE: We wrap the `process` call in `handleErrorWith` to catch any exceptions that occur during processing.
+    // This way, if one event fails to process, it won't crash the entire stream, and we can log the error instead.
+    process(event).handleErrorWith { error =>
+      IO.println(
+        s"[consumer] ERROR processing event ${event.id.value}: ${error.getMessage}"
+      )
+    }
+  }
 
   def stream(queue: Queue[IO, Event]): Stream[IO, Unit] = {
     val maxParallelism = 5
@@ -26,5 +40,5 @@ object EventConsumer:
     Stream
       .fromQueueUnterminated(queue)
       // NOTE: The `parEvalMap(maxParallelism)` means that up to X events can be processed concurrently.
-      .parEvalMap(maxParallelism)(process)
+      .parEvalMap(maxParallelism)(processSafely)
   }
